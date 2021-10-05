d1e84a6844a146fc6b7399e5aed79e8e54929a78
otmane1993 <otmomks@gmail.com>
2021-10-05 13:19:18 +0100

    animation_setup

---
+++ js/script.js
@@ -0,0 +1,103 @@
+var txt = document.querySelector('.txt-animation');
+console.log(txt);
+var Typewriter = new Typewriter(txt, {
+  loop: false,
+  delay: 45,
+});
+
+Typewriter
+  .pauseFor(300)
+  .typeString('I am Otmane KSAANI')
+  .pauseFor(1000)
+  .typeString(' I am a web developper full stack')
+  .pauseFor(1000)
+  .deleteChars(33)
+  .pauseFor(1000)
+  .typeString(' I am a web developper front end')
+  .pauseFor(1000)
+  .deleteChars(32)
+  .pauseFor(1000)
+  .typeString(' I am a web developper back end')
+  .pauseFor(1000)
+  .deleteChars(31)
+  .start();
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*const str1=" a Developper web Full Stack";
+const str2=" a Developper Back End";
+const str3=" a Developper Front End";
+function extractChar(str,index)
+{
+    return str.slice(index,index+1);
+}
+let  span=document.querySelector("#span");
+var str="";
+span.style.color='red';
+for(let i=0;i<str1.length;i++)
+{
+    setTimeout(function(){str+=extractChar(str1,i);
+        span.innerHTML=str;},200*i);
+}
+setTimeout(function(){span.innerHTML="";
+    span.style.color='green';},
+    str1.length*200);
+let stri="";
+for(let i=0;i<str2.length;i++)
+{
+    setTimeout(function(){stri+=extractChar(str2,i);
+        span.innerHTML=stri;},200*(i+str1.length));
+}
+setTimeout(function(){span.innerHTML="";
+    span.style.color='black';},
+    (str1.length+str2.length)*200);
+let strii="";
+for(let i=0;i<str3.length;i++)
+{
+    setTimeout(function(){strii+=extractChar(str3,i);
+        span.innerHTML=strii;},200*(i+str1.length+str2.length));
+}*/
+
+
+
+
+
+
